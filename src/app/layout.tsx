2cb95c1d6efecb2c63de9d417a1dac5b934c2f45
Davi-604 <davimedeiroscv100@gmail.com>
2024-04-20 14:58:44 -0300

    Adding README file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="pt-br">
             <body
                 className={cn(
                     'min-h-screen bg-background font-sans antialiased',

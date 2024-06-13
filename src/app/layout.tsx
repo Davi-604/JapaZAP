6633478ab457bbcb4a14ab9e0568a8f134caaaff
Davi-604 <davimedeiroscv100@gmail.com>
2024-06-13 14:13:08 -0300

    adjusting the zoom bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="pt-br">
             <head>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body
                 className={cn(

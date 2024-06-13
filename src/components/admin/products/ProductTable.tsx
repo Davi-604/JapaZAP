41debf3f1ecb2426dcfb9b0f91812422c66ed0ed
Davi-604 <davimedeiroscv100@gmail.com>
2024-06-13 14:07:43 -0300

    adjusting some images display and big texts

--- src/components/admin/products/ProductTable.tsx
+++ src/components/admin/products/ProductTable.tsx
@@ -37,7 +37,7 @@ export const ProductTable = ({
                         <TableCell>
                             <img
                                 src={product.image}
-                                className="max-w-[70px] sm:max-w-[110px] rounded-md"
+                                className="w-[70px] h-[50px] sm:w-[110px] sm:h-[70px] object-cover rounded-md"
                             />
                         </TableCell>
                         <TableCell>{product.name}</TableCell>
